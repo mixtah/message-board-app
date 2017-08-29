0f8209128d930060d5733d8e006a8e0c8fef84a9
Michael Bauer <da1da2da3ofme@gmail.com>
2017-08-29 21:03:43 +1000

    Added Messages and Topics models

---
+++ Messages.py
@@ -0,0 +1,107 @@
+'''
+Created on 29 Aug 2017
+
+@author: Michael
+'''
+from datetime import datetime
+import time
+import SQLiteAdapter as db
+
+class Message(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,id=None,username=None,message=None,reply_to=None,topic=None,
+                 modified=0,likes=0,dislikes=0,timestamp=datetime.now()):
+        '''
+        Constructor
+        '''
+        self.id = id
+        self.username = username
+        self.message = message
+        self.reply_to = reply_to
+        self.topic = topic
+        self.modified = modified
+        self.likes = likes
+        self.dislikes = dislikes
+        self.timestamp = timestamp
+        
+    def save(self):
+        '''
+            Adds this object to the database if no id exists, otherwise it updates.
+            This must be called after any direct modification to this object in order to 
+            make it permanent.
+        '''
+        if self.id==None:
+            #Add
+            if self.timestamp == None or len(self.timestamp)==0:
+                self.timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+                
+            res = db.QUERY("INSERT INTO messages (username,message,reply_to,topic,modified,likes,dislikes) VALUES (?,?,?,?,?,?,?)",
+                           (self.username,self.message,self.reply_to,self.topic,self.modified,self.likes,self.dislikes))
+            if res:
+                self.id=res
+                return True
+        else:
+            #Update
+            res = db.QUERY("UPDATE messages SET username=?,message=?,reply_to=?,topic=?,modified=?,likes=?,dislikes=? WHERE id=?)",
+                           (self.username,self.message,self.reply_to,self.topic,self.modified,self.likes,self.dislikes,self.id))
+            
+        if res:
+            return True
+        else:
+            return False
+        
+def get(id=None):
+    ''' Gets a Message given it's id
+    '''
+    if id==None:
+        return None
+    
+    res = db.QUERY('SELECT FROM messages WHERE id=?',(id,))
+    if len(res)>0:
+        return Message(**res[0])
+    else:
+        return None
+    
+def getAll(limit=-1):
+    ''' Get a list of all Messages '''
+    if limit>0:
+        res = db.QUERY('SELECT FROM messages LIMIT ?',(limit))
+    else:
+        res = db.QUERY('SELECT FROM messages')
+    rlist = []
+    for values in res:
+        rlist.append(Message(**values))
+    return rlist
+    
+def getFiltered(filters={}, limit=-1):
+    ''' Get a list of filtered Messages '''
+    
+    keys = filters.keys()
+    values = filters.values()
+    query = 'SELECT FROM messages WHERE '
+    for key in keys:
+        query = query + key + '=?, '
+    query = query[:-1]
+    if limit>0:
+        res = db.QUERY(query + ' LIMIT ?',(limit))
+    else:
+        res = db.QUERY(query,tuple(values))
+    rlist = []
+    for values in res:
+        rlist.append(Message(**values))
+    return rlist
+
+def delete(id=None):
+    ''' Deletes a Message given it's id
+    '''
+    if id==None:
+        return None
+    res = db.QUERY('DELETE FROM messages WHERE id=?',(id,))
+    if res>0:
+        return True
+    else:
+        return False
---
+++ Topics.py
@@ -0,0 +1,105 @@
+'''
+Created on 29 Aug 2017
+
+@author: Michael
+'''
+from datetime import datetime
+import time
+import SQLiteAdapter as db
+
+class Topic(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,id=None,username=None,subject=None,description=None,
+                 modified=0,likes=0,dislikes=0,timestamp=datetime.now()):
+        '''
+        Constructor
+        '''
+        self.id = id
+        self.username = username
+        self.subject = subject
+        self.description = description
+        self.modified = modified
+        self.likes = likes
+        self.dislikes = dislikes
+        self.timestamp = timestamp
+        
+    def save(self):
+        '''
+            Adds this object to the database if no id exists, otherwise it updates.
+            This must be called after any direct modification to this object in order to 
+            make it permanent.
+        '''
+        if self.id==None:
+            #Add
+            if self.timestamp == None or len(self.timestamp)==0:
+                self.timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+                
+            res = db.QUERY("INSERT INTO topics (username,subject,description,modified,likes,dislikes) VALUES (?,?,?,?,?,?)",
+                           (self.username,self.subject,self.description,self.modified,self.likes,self.dislikes))
+            if res:
+                self.id=res
+                return True
+        else:
+            #Update
+            res = db.QUERY("UPDATE topics SET username=?,subject=?,description=?,modified=?,likes=?,dislikes=? WHERE id=?)",
+                           (self.username,self.subject,self.description,self.modified,self.likes,self.dislikes,self.id))
+            
+        if res:
+            return True
+        else:
+            return False
+        
+def get(id=None):
+    ''' Gets a Topic given it's id
+    '''
+    if id==None:
+        return None
+    
+    res = db.QUERY('SELECT FROM topics WHERE id=?',(id,))
+    if len(res)>0:
+        return Topic(**res[0])
+    else:
+        return None
+    
+def getAll(limit=-1):
+    ''' Get a list of all Topics '''
+    if limit>0:
+        res = db.QUERY('SELECT FROM topics LIMIT ?',(limit))
+    else:
+        res = db.QUERY('SELECT FROM topics')
+    rlist = []
+    for values in res:
+        rlist.append(Topic(**values))
+    return rlist
+    
+def getFiltered(filters={}, limit=-1):
+    ''' Get a list of filtered Topics '''
+    
+    keys = filters.keys()
+    values = filters.values()
+    query = 'SELECT FROM topics WHERE '
+    for key in keys:
+        query = query + key + '=?, '
+    query = query[:-1]
+    if limit>0:
+        res = db.QUERY(query + ' LIMIT ?',(limit))
+    else:
+        res = db.QUERY(query,tuple(values))
+    rlist = []
+    for values in res:
+        rlist.append(Topic(**values))
+    return rlist
+
+def delete(id=None):
+    ''' Deletes a Topic given it's id '''
+    if id==None:
+        return None
+    res = db.QUERY('DELETE FROM topics WHERE id=?',(id,))
+    if res>0:
+        return True
+    else:
+        return False
